afc5c45592d6c4e4fb23e963f290b495e2e02a38
Mikhail Artsiushenka <brom128@gmail.com>
2015-01-21 10:31:51 +0300

    making marks fragmet

--- app/src/main/java/net/martp/mihail/mumhelper/MarksFragment.java
+++ app/src/main/java/net/martp/mihail/mumhelper/MarksFragment.java
@@ -10,12 +10,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MarksFragment extends Fragment {
 
+    private static ArrayList<String> arrayListMarks = new ArrayList<>();
 
     public MarksFragment() {
         // Required empty public constructor
@@ -34,8 +43,6 @@ public class MarksFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
         ParseDataMarksAsyncTask parseDadaNewsAsyncTask = new ParseDataMarksAsyncTask();
         parseDadaNewsAsyncTask.execute();
         OutToActivity outToActivity = new OutToActivity();
@@ -68,9 +75,43 @@ public class MarksFragment extends Fragment {
         }
 
 
+        private ArrayList<String> getArrayMarks() {
+            return arrayListMarks;
+        }
+
+        private void setArrayMarks(ArrayList<String> arrayMarksf) {
+            arrayListMarks = arrayMarksf;
+        }
         @Override
         protected Void doInBackground(Void... params) {
 
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("http://martp.net/muu/learning-card.html")
+                        .data("act", "regnum").data("id", "id").data("regnum", "20090312012423").post();
+            } catch (IOException e) {
+                System.out.println("Ошибка подключени к сети " + getClass().getSimpleName());
+               // return;
+            }
+
+            Element table = doc.select("table").get(1);
+            Elements rows = table.select("tr");
+
+            for (int i = 0; i < rows.size(); i++) {
+                Element row = rows.get(i);
+                Elements cols = row.select("td");
+                System.out.print(cols.get(0).text());
+                System.out.print(" ");
+                System.out.print(cols.get(1).text());
+                System.out.print(" ");
+
+                String body = cols.get(2).getElementsByTag("img").attr("alt");
+                if (body.equals("зачтено") || body.equals("незачтено"))
+                    System.out.print(body);
+                else System.out.print(cols.get(2).text());
+                System.out.print(" ");
+                System.out.println(cols.get(3).text());
+            }
             return null;
         }
 
