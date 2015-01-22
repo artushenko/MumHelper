161cd5c199670743fbdbe771ce35ce00a35d134a
Mikhail Artsiushenka <brom128@gmail.com>
2015-01-22 12:38:11 +0300

    make marks fragment

--- app/src/main/java/net/martp/mihail/mumhelper/MarkStructure.java
+++ app/src/main/java/net/martp/mihail/mumhelper/MarkStructure.java
@@ -4,17 +4,21 @@ package net.martp.mihail.mumhelper;
  * Created by Mihail on 24.12.2014.
  */
 public class MarkStructure {
+    boolean status;
     String nameOfDiscipline;
     String formOfControl;
     String mark;
     String date;
 
-    public MarkStructure(String nameOfDiscipline, String formOfControl, String mark, String date) {
+    public MarkStructure(boolean status, String nameOfDiscipline, String formOfControl, String mark, String date) {
+        this.status=status;
         this.nameOfDiscipline = nameOfDiscipline;
         this.formOfControl = formOfControl;
         this.mark = mark;
         this.date = date;
     }
+
+    public boolean getStatud() {return status;}
 
     public String getNameOfDiscipline() {
         return nameOfDiscipline;
--- app/src/main/java/net/martp/mihail/mumhelper/MarksFragment.java
+++ app/src/main/java/net/martp/mihail/mumhelper/MarksFragment.java
@@ -4,11 +4,17 @@ package net.martp.mihail.mumhelper;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -25,7 +31,7 @@ import java.util.ArrayList;
  */
 public class MarksFragment extends Fragment {
 
-    private static ArrayList<String> arrayListMarks = new ArrayList<>();
+    private static ArrayList<MarkStructure> arrayListMarks = new ArrayList<>();
 
     public MarksFragment() {
         // Required empty public constructor
@@ -46,7 +52,7 @@ public class MarksFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ParseDataMarksAsyncTask parseDataMarksAsyncTask = new ParseDataMarksAsyncTask();
         parseDataMarksAsyncTask.execute();
-       // OutToActivity outToActivity =  new OutToActivity();
+        // OutToActivity outToActivity =  new OutToActivity();
         new OutToActivity();
     }
 
@@ -58,7 +64,9 @@ public class MarksFragment extends Fragment {
 
     private class ParseDataMarksAsyncTask extends AsyncTask<Void, Integer, Void> {
 
-        public ProgressDialog dialog;
+        ProgressDialog dialog;
+        TableLayout queryTableLayout;
+        Context context;
 
         @Override
         protected void onPreExecute() {
@@ -73,17 +81,18 @@ public class MarksFragment extends Fragment {
 
         }
 
-
-        private ArrayList<String> getArrayMarks() {
+        private ArrayList<MarkStructure> getArrayMarks() {
             return arrayListMarks;
         }
 
-        private void setArrayMarks(ArrayList<String> arrayMarksf) {
+        private void setArrayMarks(ArrayList<MarkStructure> arrayMarksf) {
             arrayListMarks = arrayMarksf;
         }
 
         @Override
         protected Void doInBackground(Void... params) {
+
+            ArrayList<MarkStructure> arrayListMarksLocal = new ArrayList<>();
 
             Document doc = null;
             try {
@@ -98,22 +107,47 @@ public class MarksFragment extends Fragment {
             Element table = doc.select("table").get(1);
             Elements rows = table.select("tr");
 
-            for (int i = 0; i < rows.size(); i++) {
+            String status = "";
+
+            for (int i = 1; i < rows.size(); i++) {
                 Element row = rows.get(i);
                 Elements cols = row.select("td");
+
                 System.out.print(cols.get(0).text());
                 System.out.print(" ");
                 System.out.print(cols.get(1).text());
                 System.out.print(" ");
 
+
                 String body = cols.get(2).getElementsByTag("img").attr("alt");
-                if (body.equals("зачтено") || body.equals("незачтено"))
+                if (body.equals("зачтено") || body.equals("незачтено")) {
+                    status = body;
                     System.out.print(body);
-                else System.out.print(cols.get(2).text());
+                } else {
+                    System.out.print(cols.get(2).text());
+                    status = cols.get(2).text();
+                }
                 System.out.print(" ");
                 System.out.println(cols.get(3).text());
+                arrayListMarksLocal.add(new MarkStructure(getStatusDiscipline(status), cols.get(0).text(), cols.get(1).text(), cols.get(2).text(), cols.get(3).text()));
             }
+            setArrayMarks(arrayListMarksLocal);
+
+            //         System.out.println("1111111111111");
+
             return null;
+        }
+
+        private boolean getStatusDiscipline(String mark) {
+            if (mark.equals("зачтено")) return true;
+            if (mark.equals("")) return false;
+
+            try {
+                if (Integer.parseInt(mark) >= 4) return true;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            return false;
         }
 
         @Override
@@ -121,6 +155,46 @@ public class MarksFragment extends Fragment {
             super.onPostExecute(aVoid);
             dialog.dismiss();
             Toast.makeText(getActivity(), "Reading marks is comlpete", Toast.LENGTH_SHORT).show();
+            ArrayList<MarkStructure> arrayListMarkLocal = new ArrayList<>();
+            arrayListMarkLocal = getArrayMarks();
+
+            MarkStructure mark;
+            queryTableLayout = (TableLayout) getView().findViewById(R.id.markTable);
+            for (int i = 0; i < arrayListMarkLocal.size(); i++) {
+                mark = arrayListMarkLocal.get(i);
+                makeMarksLine(mark.getNameOfDiscipline(), mark.getFormOfControl(),mark.getMark(),mark.getDate(), mark.getStatud(),i);
+            }
         }
+
+        private void makeMarksLine(String nameOfDiscipline, String formOfControl, String mark, String date, boolean status, int index) {
+            context=getView().getContext();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View newTagView = inflater.inflate(R.layout.marks_list_item, null);
+
+
+            ImageView statusPredmet = (ImageView) newTagView.findViewById(R.id.StatusPredmetImageView);
+            Bitmap srcBitmapLocal = BitmapFactory.decodeResource(context.getApplicationContext().getResources(), R.drawable.tick);
+            if (!status) {
+                srcBitmapLocal = BitmapFactory.decodeResource(context.getApplicationContext().getResources(),
+                        R.drawable.cross);
+            }
+            statusPredmet.setImageBitmap(srcBitmapLocal);
+
+            TextView textPredmet = (TextView) newTagView.findViewById(R.id.nameOfDiscipline);
+            textPredmet.setText(nameOfDiscipline);
+
+            TextView textVidPredmate = (TextView) newTagView.findViewById(R.id.formOfControl);
+            textVidPredmate.setText(formOfControl);
+
+            TextView textMarks = (TextView) newTagView.findViewById(R.id.mark);
+            textMarks.setText(mark);
+
+            TextView textDateEkz = (TextView) newTagView.findViewById(R.id.date);
+            textDateEkz.setText(date);
+
+            queryTableLayout.addView(newTagView, index);
+        }
+
+
     }
 }
